7e5d0c8790bbc6c5bdc8e74e99690cb17d0d3249
ihsgnodoay <2111267500@qq.com>
2023-08-14 11:27:48 +0800

    feat: 更新头像

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,9 @@
 const headerNavLinks = [
   // { href: '/', title: 'home' },
-  { href: '/blog', title: 'notes' },
-  { href: '/projects', title: 'projects' },
-  { href: '/about', title: 'about' },
+  // { href: '/blog', title: 'notes' },
+  { href: '/blog', title: '帖子' },
+  // { href: '/projects', title: 'projects' },
+  // { href: '/about', title: 'about' },
 ];
 
 export default headerNavLinks;
